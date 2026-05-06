6c49bc3b317b4cda4643ef5728ff1ec4ee8ac3fc
Amr <asym17182@gmail.com>
2026-05-06 23:22:54 +0400

    training a 1280 yolov26s

--- scripts/training.py
+++ scripts/training.py
@@ -3,59 +3,25 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+# Make sure your ultrulytics json file has the right paths (the location of the file is different per distro)
+# place YOLO_DATASET (the folder outputted by create_structure.py) in the directory root
+# make sure paths in data.yaml are good
+
+model = YOLO("yolo26s.pt")  # load a pretrained model (recommended for training)
 
 results = model.train(
   data="YOLO_DATASET/data.yaml",
   epochs=600,
-  imgsz=640, # or 640
+  imgsz=1280, # or 640
   rect=True,
   patience=50,
-  batch=0.90,
+  batch=0.85,
   save=True,
   save_period=30,
   exist_ok=True,
   profile=True, # For tensorRT & ONNX
 
   project=os.path.join(SCRIPT_DIR, 'training'),
-  name='yolo11n_640',
-  device=0
-)
-
-model = YOLO("yolo11s.pt")  # load a pretrained model (recommended for training)
-
-results = model.train(
-  data="YOLO_DATASET/data.yaml",
-  epochs=600,
-  imgsz=640, # or 640
-  rect=True,
-  patience=50,
-  batch=0.90,
-  save=True,
-  save_period=30,
-  exist_ok=True,
-  profile=True, # For tensorRT & ONNX
-
-  project=os.path.join(SCRIPT_DIR, 'training'),
-  name='yolo11s_640',
-  device=0
-)
-
-model = YOLO("yolo11m.pt")  # load a pretrained model (recommended for training)
-
-results = model.train(
-  data="YOLO_DATASET/data.yaml",
-  epochs=600,
-  imgsz=640, # or 640
-  rect=True,
-  patience=50,
-  batch=0.90,
-  save=True,
-  save_period=30,
-  exist_ok=True,
-  profile=True, # For tensorRT & ONNX
-
-  project=os.path.join(SCRIPT_DIR, 'training'),
-  name='yolo11m_640',
+  name='yolo11m_1280',
   device=0
 )
